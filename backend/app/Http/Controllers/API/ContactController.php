856f998c9c2199951be869c742e358631d6196bb
Anatoliy Zakhryapin <anatoli.zakhryapin@gmail.com>
2024-04-04 22:18:41 +0200

    ADD function index to ContactController

--- backend/app/Http/Controllers/API/ContactController.php
+++ backend/app/Http/Controllers/API/ContactController.php
@@ -3,9 +3,17 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\Contact;
 use Illuminate\Http\Request;
 
 class ContactController extends Controller
 {
-    //
+    public function index()
+    {
+        // Recupera tutti gli utenti dal database
+        $contacts = Contact::all();
+
+        // Restituisci tutti contatti come risposta JSON
+        return response()->json(['contacts' => $contacts]);
+    }
 }
