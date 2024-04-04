736b2f9e470ca0debe94984fbc6206b8d96439c1
Anatoliy Zakhryapin <anatoli.zakhryapin@gmail.com>
2024-04-04 20:45:26 +0200

    ADD update function to UserController

--- backend/app/Http/Controllers/API/UserController.php
+++ backend/app/Http/Controllers/API/UserController.php
@@ -51,4 +51,31 @@ class UserController extends Controller
 
         return response()->json(['user' => $user], 201);
     }
+
+    public function update(Request $request, $id)
+    {
+
+        // Trova l'utente da aggiornare
+        $user = User::find($id);
+
+        // Se l'utente non esiste, restituisci un messaggio di errore
+        if (!$user) {
+            return response()->json(['error' => 'User not found'], 404);
+        }
+
+        // Validazione dei dati inviati
+        $validatedData = $request->validate([
+            'name' => 'string|max:255',
+            'surname' => 'string|max:255',
+            'email' => 'email|max:255|unique:users,email,' . $id,
+            'password' => 'string|min:8',
+            'role' => 'string|max:255',
+        ]);
+
+        // Aggiorna le informazioni dell'utente con i dati validati
+        $user->update($validatedData);
+
+        // Restituisci i dati aggiornati dell'utente
+        return response()->json(['message' => 'User updated successfully', 'user' => $user]);
+    }
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -26,6 +26,7 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
 Route::get('/users', [UserController::class, 'index']);
 Route::get('/users/{id}', [UserController::class, 'show']);
 Route::post('/users', [UserController::class, 'store']);
+Route::put('/users/{id}', [UserController::class, 'update']);
 
 
 Route::post('/login', [LoginController::class, 'login']);
