c66a3426b0c2acf37941c2489a6a2835bc6efeeb
Anatoliy Zakhryapin <anatoli.zakhryapin@gmail.com>
2024-04-06 11:58:42 +0200

    ADD register function to UserController

--- backend/app/Http/Controllers/API/UserController.php
+++ backend/app/Http/Controllers/API/UserController.php
@@ -8,6 +8,9 @@ use App\Http\Requests\User\UpdateRequest;
 use App\Http\Resources\User\UserResource;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Hash;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class UserController extends Controller
 {
@@ -33,13 +36,58 @@ class UserController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreRequest $request)
+    // public function store(StoreRequest $request)
+    // {
+    //     $data = $request->validated();
+
+    //     $new_user = User::create($data);
+
+    //     return UserResource::make($new_user);
+    // }
+
+    public function register(Request $request)
     {
-        $data = $request->validated();
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8',
+            'role' => 'required|string|max:255',
+        ]);
 
-        $new_user = User::create($data);
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 422);
+        }
 
-        return UserResource::make($new_user);
+
+        // Ottenere l'utente autenticato
+        $user = auth()->user();
+
+        // Verificare se l'utente ha il ruolo di super-admin
+        if ($user->role === 'super-admin') {
+            // Se l'utente ha il ruolo di super-admin, permettere qualsiasi ruolo per il nuovo utente
+            $allowedRoles = ['admin', 'super-admin', 'maintainer', 'basic'];
+        } else {
+            // Altrimenti, permettere solo i ruoli 'maintainer' e 'basic'
+            $allowedRoles = ['maintainer', 'basic'];
+        }
+
+        // Verificare se il ruolo richiesto per il nuovo utente è consentito
+        if (!in_array($request->role, $allowedRoles)) {
+            return response()->json(['error' => 'Unauthorized role'], 403);
+        }
+
+        $newUser = User::create([
+            'name' => $request->input('name'),
+            'surname' => $request->input('surname'),
+            'email' => $request->input('email'),
+            'password' => Hash::make($request->input('password')),
+            'role' => $request->role,
+        ]);
+
+        // Generare il token JWT per il nuovo utente registrato
+        $token = JWTAuth::fromUser($newUser);
+
+        return response()->json(compact('newUser', 'token'), 201);
     }
 
     /**
