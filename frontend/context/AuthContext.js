6ae9ffc1598333e46eda5dfe0c5a04c7bdf00c80
selight <ghiwotselam@gmail.com>
2024-04-10 12:52:18 +0200

    disabled api integration

--- frontend/context/AuthContext.js
+++ frontend/context/AuthContext.js
@@ -10,7 +10,7 @@ export const AuthProvider = ({ children }) => {
         const checkLocalStorage = () => {
             const userData = localStorage.getItem('user');
             if (userData) {
-                setUser(JSON.parse(userData).user);
+                setUser(JSON.parse(userData));
             }
             setIsLoading(false);
         };
@@ -20,32 +20,32 @@ export const AuthProvider = ({ children }) => {
 
     const login = async (email, password) => {
         try {
-            const response = await fetch('/api/signIn', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ email, password }),
-            });
-            if (response.ok) {
-                const { access_token } = await response.json();
-                const userResponse = await fetch('/api/me', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                        Authorization: `Bearer ${access_token}`,
-                    },
-                });
-                if (userResponse.ok) {
-                    const userData = await userResponse.json();
-                    localStorage.setItem('user', JSON.stringify({ access_token, user: userData }));
+            // const response = await fetch('/api/signIn', {
+            //     method: 'POST',
+            //     headers: {
+            //         'Content-Type': 'application/json',
+            //     },
+            //     body: JSON.stringify({ email, password }),
+            // });
+            // if (response.ok) {
+            //     const { access_token } = await response.json();
+            //     const userResponse = await fetch('/api/me', {
+            //         method: 'POST',
+            //         headers: {
+            //             'Content-Type': 'application/json',
+            //             Authorization: `Bearer ${access_token}`,
+            //         },
+            //     });
+            //     if (userResponse.ok) {
+                    const userData = { email };
+                    localStorage.setItem('user', JSON.stringify({ user: { email } }));
                     setUser(userData);
-                } else {
-                    throw new Error('Failed to fetch user data');
-                }
-            } else {
-                throw new Error('Failed to login');
-            }
+            //     } else {
+            //         throw new Error('Failed to fetch user data');
+            //     }
+            // } else {
+            //     throw new Error('Failed to login');
+            // }
         } catch (error) {
             throw error;
         }
